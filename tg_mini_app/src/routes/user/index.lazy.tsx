c6045475224d24ee09f7215bbc470fb6d1f4be48
Davyd Poplyonkin <dp.8cbpm@gmail.com>
2026-05-25 17:16:11 +0800

    feat: add QR code generation buttons on the user home page

--- tg_mini_app/src/routes/user/index.lazy.tsx
+++ tg_mini_app/src/routes/user/index.lazy.tsx
@@ -1,25 +1,25 @@
 import { createLazyFileRoute, useNavigate } from '@tanstack/react-router'
-import { Table } from '@mantine/core'
+import { Timeline, Text, ActionIcon, Group } from '@mantine/core'
 import { useQuery } from '@tanstack/react-query'
+import { IconQrcode } from '@tabler/icons-react'
+import dayjs from 'dayjs'
+import utc from 'dayjs/plugin/utc'
+import customParseFormat from 'dayjs/plugin/customParseFormat'
 
 import { UserAppointmentsQueryOptions } from './index.queries'
 import { MainButton } from '../../components/MainButton'
 
+dayjs.extend(customParseFormat)
+dayjs.extend(utc)
+
 export const Route = createLazyFileRoute('/user/')({
   component: UserList,
-})
+});
 
 function UserList() {
   const navigate = useNavigate();
 
   const { data: appointments } = useQuery(UserAppointmentsQueryOptions);
-
-  const rows = appointments?.map((appointment) => (
-    <Table.Tr key={appointment.id}>
-      <Table.Td>{appointment.date}</Table.Td>
-      <Table.Td>{appointment.time}</Table.Td>
-    </Table.Tr>
-  ));
 
   return (
     <>
@@ -28,15 +28,30 @@ function UserList() {
         isActive={true}
         callback={() => {navigate({ to: '/user/booking' })}}
       />
-      <Table>
-        <Table.Thead>
-          <Table.Tr>
-            <Table.Th>Date</Table.Th>
-            <Table.Th>Time</Table.Th>
-          </Table.Tr>
-        </Table.Thead>
-        <Table.Tbody>{rows}</Table.Tbody>
-      </Table>
+      <Timeline bulletSize={16} lineWidth={2} active={-1} mb='md'>
+        {appointments?.map((appt) => (
+          <Timeline.Item key={appt.id}>
+            <Group justify="space-between" wrap="nowrap">
+              <div>
+                <Text>{dayjs.utc(appt.date).format('ddd, MMM D')}</Text>
+                <Text
+                  size='sm'
+                  c='dimmed'
+                  style={{ fontVariantNumeric: 'tabular-nums' }}
+                >
+                  {dayjs.utc(appt.time, 'HH:mm:ss').format('HH:mm')}
+                </Text>
+              </div>
+              <ActionIcon variant='filled' size='lg'>
+                <IconQrcode
+                  stroke={2}
+                  style={{ width: '90%', height: '90%' }}
+                />
+              </ActionIcon>
+            </Group>
+          </Timeline.Item>
+        ))}
+      </Timeline>
     </>
   );
 }
